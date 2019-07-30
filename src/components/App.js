22154ef24ef4ee5a47d906e795d2b18b3f3ada77
Tomasz Klepacki <tomasz.klepacki@iforbet.pl>
2019-07-30 15:33:50 +0200

    Added dummy reducer, semantic ui and list component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import RecipeList from './RecipeList';
+
+const App = () => {
+    return (
+        <RecipeList />
+        );
+};
+
+export default App;
---
+++ src/components/RecipeList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class RecipeList extends React.Component {
+    render() {
+        return <div>LIST</div>
+    }
+}
+ export default RecipeList;
