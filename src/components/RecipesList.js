0aa68545380ff6bd8e240bfb0a2f94ccd9ee5fac
Tomasz Klepacki <t.klepacki@gmail.com>
2019-08-02 00:56:12 +0200

    Added multicheckbox select with unique ingredients

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { fetchRecipes } from "../actions";
+import ReactMultiSelectCheckboxes from "react-multiselect-checkboxes";
 
-import Recipe from './Recipe';
+import Recipe from "./Recipe";
 
 class RecipesList extends React.Component {
+  state = {
+    userIngredients: {},
+  }
+
   componentDidMount() {
     this.props.fetchRecipes();
+  };
+
+  uniqueIngredients = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
+
+  onInputChange = (e) => {
+    const user = e.value
+    console.log(user)
   }
+
   render() {
-    console.log(this.props.recipes)
-    return <Recipe />
+    const { recipes } = this.props;
+    const ingredients = recipes
+      .map(item => {
+        return item.ingredients;
+      })
+      .join(", ")
+      .split(", ")
+      .sort();
+    const options = ingredients
+      .filter(this.uniqueIngredients)
+      .map((item, index) => {
+        return { label: item, value: index };
+      });
+    console.log(this.state.userIngredients)
+    return (
+      <Fragment>
+        <ReactMultiSelectCheckboxes
+          options={options}
+          getDropdownButtonLabel={(e) => this.onInputChange(e)}
+        />
+        <Recipe />
+      </Fragment>
+    );
   }
-};
+}
 
 const mapStateToProps = state => {
   return { recipes: state.recipes };
