34de83711b83f8cee5acfc4cae01df318667a623
Tomasz Klepacki <t.klepacki@gmail.com>
2019-08-04 22:52:56 +0200

    Updates

---
+++ precache-manifest.426e497464509e5878c222ef6e8c2cda.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04fbfb7c07aeb412e927de15f1c6b9d",
+    "url": "/recipe-puppy/index.html"
+  },
+  {
+    "revision": "4890fa180e7b65f43356",
+    "url": "/recipe-puppy/static/js/2.c7accca9.chunk.js"
+  },
+  {
+    "revision": "794b5a58297e045f5699",
+    "url": "/recipe-puppy/static/js/main.465a18c2.chunk.js"
+  },
+  {
+    "revision": "8740b196878e4f90cad7",
+    "url": "/recipe-puppy/static/js/runtime~main.ea77c55d.js"
+  }
+]);
